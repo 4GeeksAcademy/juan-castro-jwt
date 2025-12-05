9335c78645a827745b43318c4b738a6b8bc75434
juan <130606010+juancastrop@users.noreply.github.com>
2025-12-05 23:30:30 +0000

    creacion de db y crear usuario

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,12 +5,13 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_bcrypt import Bcrypt
 
 api = Blueprint('api', __name__)
 
 # Allow CORS requests to this API
 CORS(api)
-
+bcrypt = Bcrypt()
 
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
@@ -20,3 +21,54 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route("/create_user", methods=["POST"])
+def create_user():
+
+    #recuperar la data entrante del json 
+    data = request.get_json()
+
+    #verificar si tiene infomacion la data
+    if not data:
+        return jsonify({"msg":"no hay data, la proxima presta atencion"}), 400
+    
+    #recuperar la varible
+    name = data.get("name")
+    email = data.get("email")
+    is_active = data.get("is_active", True )
+    password = data.get("password")
+
+    #verificar si las variables tienen contenido
+    if not name or not email or not password:
+         return jsonify({"msg":"algun dato te falto"}), 400
+    
+    #confirmar si ese usuario existe en la base de datos buscandolo por email
+    busqueda = User.query.filter_by(email=email).first()
+
+    #si existe mandar devolucion con informacion que existe y si no continuar con la creacion
+    if busqueda:
+        return jsonify({"msg":"este email ya existe"}), 400 
+
+    #hashear password
+    passhash = bcrypt.generate_password_hash(password).decode("utf-8")
+
+
+
+    #crear nuevo usuario
+    new_user = User(
+        name=name,
+        email=email,
+        password=passhash,
+        is_active=is_active
+        )
+
+    #anexarlo a la sesion
+    db.session.add(new_user)
+
+    #commitiar la sesion
+    db.session.commit()
+
+    #retornar los datos del usuario
+    return jsonify({"msg":"todo un exito", "nuevo_usuario": new_user.serialize()}), 201
+
+
