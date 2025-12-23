a86e304dc912a9c105daf029d44207d4dcb28e97
jenifferalejandracollazos-droid <jenifferalejandracollazos@gmail.com>
2025-12-23 22:43:55 +0000

    copnflicto resuelto con importacion perdida de trainer

--- src/front/pages/Trainer.jsx
+++ src/front/pages/Trainer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+export const Trainer = () => <div>Vista de Trainer</div>;import React from "react";
 import { useNavigate } from "react-router-dom";
 import Logout from "../components/Logout";
 import { useEffect,useState } from "react";
