48d859c1386975c4e7c3b22bcd001ab27b865e4d
juan <130606010+juancastrop@users.noreply.github.com>
2026-01-06 00:21:01 +0000

    ajuste de los ejercicos conla api

--- src/front/pages/Admin.jsx
+++ src/front/pages/Admin.jsx
@@ -42,37 +42,87 @@ const Admin = () => {
          {!loading && exercises.length === 0 && <p>No hay ejercicios cargados.</p>}
 
          <div className="cards-container">
-            {exercises.map((ele) => (
-               <div className="card" style={{ width: "18rem" }} key={ele.id || ele.uid || ele.name}>
-                  <div className="card">
-                     <h5 className="card-header">{ele.name}</h5>
+            {exercises.map((ele, idx) => {
+               const instrModalId = `staticBackdrop-${idx}`;
+               const safetyModalId = `staticBackdrop2-${idx}`;
+               const instrLabelId = `staticBackdropLabel-${idx}`;
+               const safetyLabelId = `staticBackdropLabel2-${idx}`;
+               return (
+                  <div className="card" style={{ width: "30rem" }} key={ele.id || ele.uid || ele.name}>
                      <div className="card-body">
-                        <h5 className="card-title">Special title treatment</h5>
-                        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                        <h5 className="card-title">{ele.name}</h5>
+                        <ul className="list-group list-group-flush">
+                           <li className="list-group-item">Tipo: {ele.type}</li>
+                           <li className="list-group-item">Musculo que trabaja: {ele.muscle}</li>
+                           <li className="list-group-item">Dificultad: {ele.difficulty}</li>
+                        </ul>
+
+                        {/* Button trigger modal */}
+                        <button
+                           type="button"
+                           className="btn btn-primary"
+                           data-bs-toggle="modal"
+                           data-bs-target={`#${instrModalId}`}
+                        >
+                           Instrucciones
+                        </button>
+                        <button
+                           type="button"
+                           className="btn btn-primary"
+                           data-bs-toggle="modal"
+                           data-bs-target={`#${safetyModalId}`}
+                        >
+                           Info de seguridad
+                        </button>
+
+                        {/* Modal */}
+                        <div
+                           className="modal fade"
+                           id={instrModalId}
+                           data-bs-backdrop="static"
+                           data-bs-keyboard="false"
+                           tabIndex={-1}
+                           aria-labelledby={instrLabelId}
+                           aria-hidden="true"
+                        >
+                           <div className="modal-dialog">
+                              <div className="modal-content">
+                                 <div className="modal-header">
+                                    <h1 className="modal-title fs-5" id={instrLabelId}>Instrucciones</h1>
+                                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                 </div>
+                                 <div className="modal-body">
+                                    {ele.instructions}
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+                        <div
+                           className="modal fade"
+                           id={safetyModalId}
+                           data-bs-backdrop="static"
+                           data-bs-keyboard="false"
+                           tabIndex={-1}
+                           aria-labelledby={safetyLabelId}
+                           aria-hidden="true"
+                        >
+                           <div className="modal-dialog">
+                              <div className="modal-content">
+                                 <div className="modal-header">
+                                    <h1 className="modal-title fs-5" id={safetyLabelId}>Info de seguridad</h1>
+                                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                 </div>
+                                 <div className="modal-body">
+                                    {ele.safety_info}
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
                      </div>
                   </div>
-                  
-                  <img
-                     src={ele.gifUrl || ele.image || ele.imageUrl || ""}
-                     className="card-img-top"
-                     alt={ele.name || "exercise"}
-                  />
-                  <div className="card-body">
-                     <h5 className="card-title">{ele.name}</h5>
-                     <p className="card-text">{ele.bodyPart || ele.target || ""}</p>
-                  </div>
-                  <div className="card">
-                     <h5 className="card-header">{ele.name}</h5>
-                     <div className="card-body">
-                        <h5 className="card-title">Special title treatment</h5>
-                        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
-                     </div>
-                  </div>
-               </div>               
-            ))}
-            
+               );
+            })}
+
          </div>
       </div>
    );
